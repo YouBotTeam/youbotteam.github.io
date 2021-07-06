700b8db208504512918bd196e3bd6f315d25e725
Antonio Santangelo <antonio.santangelo@gruppodistribuzione.faustiniana.local>
2021-07-06 16:31:39 +0200

    Fix front end

--- js/main.js
+++ js/main.js
@@ -1,215 +1,340 @@
-// Rasa Web Chat
-window.WebChat.default({
-  selector: "#webchat",
-  initPayload: "/welcome",
-  customData: { language: "en" }, // arbitrary custom data. Stay minimal as this will be added to the socket
-  socketUrl: "https://demo.weai.it",
-  socketPath: "/socket.io/",
-  showFullScreenButton: true,
-  title: "Comune di Milano",
-  profileAvatar: "./images/favicon.png",
-  subtitle: "Infoline 020202",
-  inputTextFieldHint: "Scrivi la tua richiesta...",
-  params: { storage: "session" }, // can be set to "local"  or "session". details in storage section.
-});
+var mainContainer;
+var containerCustom;
+var customPopupClassList;
+var conversationContainer;
 
-// Operazioni di editing web chat
-setTimeout(() => {
-  webChatOperation();
-
-  const mutationObserver = new MutationObserver(function (mutationsList) {
-    console.log("Mutations:", mutationsList);
-    webChatOperation();
+document.addEventListener("DOMContentLoaded", () => {
+  // Rasa Web Chat
+  window.WebChat.default({
+    selector: "#webchat",
+    initPayload: "/welcome",
+    customData: { language: "en" }, // arbitrary custom data. Stay minimal as this will be added to the socket
+    socketUrl: "https://demo.weai.it",
+    socketPath: "/socket.io/",
+    showFullScreenButton: true,
+    title: "Comune di Milano",
+    profileAvatar: "./images/favicon.png",
+    subtitle: "Infoline 020202",
+    inputTextFieldHint: "Scrivi la tua richiesta...",
+    params: { storage: "session" }, // can be set to "local"  or "session". details in storage section.
   });
 
-  mutationObserver.observe(
-    document.getElementsByClassName("rw-widget-container")[0],
-    { attributes: true }
-  );
+  // Operazioni di editing web chat
+  setTimeout(() => {
+    mainContainer = document.getElementsByClassName("rw-widget-container")[0];
+    // setWidthCustom(mainContainer.classList);
+    webChatOperation();
 
-  function webChatOperation() {
-    if (
-      document
-        .getElementById("rasaWebchatPro")
-        .getElementsByClassName("rw-widget-container")[0]
-        .classList.contains("rw-chat-open")
-    ) {
-      // Intercept click event on image
-      document.getElementsByClassName("image")[0].addEventListener(
-        "click",
-        function (ev) {
-          window.open(ev.srcElement.currentSrc, "_blank");
-        },
-        false
-      );
+    const mutationObserver = new MutationObserver((mutationsList) => {
+      var mutationsListClassList = mutationsList[0].target.classList;
+      setWidthCustom(mutationsListClassList);
+      webChatOperation();
+    });
 
-      // Button reset
-      if (!document.getElementsByClassName("button-reset").length) {
-        var buttonReset = document.createElement("button");
-        buttonReset.classList.add("button-reset");
-        var iconButtonReset = document.createElement("img");
-        iconButtonReset.classList.add("rw-default", "icon-reset");
-        iconButtonReset.setAttribute("src", "./images/resetIcon.png");
-        buttonReset.appendChild(iconButtonReset);
-        document
-          .getElementsByClassName("rw-header-buttons")[0]
-          .insertBefore(
-            buttonReset,
-            document.getElementsByClassName("rw-toggle-fullscreen-button")[0]
-          );
-        buttonReset.addEventListener("click", function () {
-          console.log("/reset");
-        });
-      }
+    mutationObserver.observe(mainContainer, { attributes: true });
+  }, 500);
+});
 
-      // Remove avatar on any msg
-      var messages = document.getElementById("rw-messages");
-      var messagesList = messages.getElementsByClassName("rw-message");
-      for (var message of messagesList) {
-        message.classList.remove("rw-with-avatar");
-        if (message.querySelector(".rw-avatar") !== null) {
-          const img = message.getElementsByTagName("img");
-          if (img.length) {
-            message.removeChild(img[0]);
-          }
-        }
-      }
+function setWidthCustom(domClassList) {
+  if (!domClassList.contains("rw-chat-open")) {
+    // mainContainer.classList.add("w-half");
+  } else {
+    // mainContainer.classList.remove("w-half");
+  }
+}
 
-      // Custom container
-      var conversationContainer = document.getElementsByClassName(
-        "rw-conversation-container"
-      )[0];
-      showOrHideChatContainer(conversationContainer, false);
+function webChatOperation() {
+  createCustomPopup("Clicca sul bottone per inziare la chat");
+  if (mainContainer.classList.contains("rw-chat-open")) {
+    clickOnImage();
+    buttonMenu();
+    // removeAvatarOnMsg();
+    customPopupClassList.add("display-none");
+    // Custom container
+    conversationContainer = document.getElementsByClassName(
+      "rw-conversation-container"
+    )[0];
+  } else {
+    customPopupClassList.add("fade-in");
+    customPopupClassList.remove("display-none");
+  }
+}
+
+function createCustomPopup(contentPopup) {
+  if (!document.querySelector(".popup-custom")) {
+    mainContainer.insertAdjacentHTML(
+      "beforeend",
+      `<div class="popup-custom ">${contentPopup}</div>`
+    );
+    customPopupClassList =
+      document.getElementsByClassName("popup-custom")[0].classList;
+  }
+}
+
+function buttonMenu() {
+  if (!document.getElementsByClassName("dropdown").length) {
+    document.getElementsByClassName("rw-header-buttons")[0].insertAdjacentHTML(
+      "beforeend",
+      `
+        <div class="dropdown">
+          <button class="button-menu">
+            <img class="rw-default icon-button-menu" src="./images/three-dots-vertical.svg"/>
+          </button>
+          <div id="dropdown-content">
+            <a id="reset-chat">Reset chat</a>
+            <a id="pillole">Pillole</a>
+          </div>
+        </div>`
+      // <a id="feedback">Feedback</a>
+    );
+
+    document.getElementById("reset-chat").addEventListener("click", () => {
+      // TODO: aggiungere la chiamata a servizio
+      console.log("/reset");
+    });
+
+    document.getElementById("pillole").addEventListener("click", () => {
+      console.log("/pillole");
+      showOrHideChatContainer(false);
       showOrHideHeaderContainer(false);
-      const containerCustom = document.createElement("div");
+      containerCustom = document.createElement("div");
       containerCustom.classList.add(
         "rw-messages-container",
         "container-custom"
       );
-      // Pillole
-      //   pilloleSection(conversationContainer, containerCustom);
-      feedbackSection(conversationContainer, containerCustom);
+      pilloleSection();
+    });
+  }
+}
+
+function openFeedbackSection() {
+  showOrHideChatContainer(false);
+  showOrHideHeaderContainer(false);
+  containerCustom = document.createElement("div");
+  containerCustom.classList.add("rw-messages-container", "container-custom");
+  feedbackSection();
+}
+
+function clickOnImage() {
+  document.getElementsByClassName("image")[0].addEventListener(
+    "click",
+    (ev) => {
+      window.open(ev.srcElement.currentSrc, "_blank");
+    },
+    false
+  );
+}
+
+function removeAvatarOnMsg() {
+  const messages = document.getElementById("rw-messages");
+  const msgReponse = messages.getElementsByClassName("rw-from-response");
+  // const messagesList = messages.getElementsByClassName("rw-message");
+  for (var message of msgReponse) {
+    message.classList.remove("rw-with-avatar");
+    if (!message.querySelector(".rw-avatar")) {
+      const img = message.getElementsByTagName("img");
+      if (img.length) {
+        message.removeChild(img[0]);
+      }
     }
   }
+}
 
-  function feedbackSection(conversationContainer, containerCustom) {
-    if (document.querySelector(".container-custom") === null) {
-      console.log(document.querySelector(".container-custom"));
-      conversationContainer.appendChild(containerCustom);
-      containerCustom.innerHTML = `
+function feedbackSection() {
+  if (!document.querySelector(".container-custom")) {
+    createCustomHeader("Lascia un feedback");
+    conversationContainer.appendChild(containerCustom);
+    containerCustom.innerHTML = `
       <div id="container-custom">
-        <div class="container-close">
-            <img id="close-icon" src="./images/x-lg.svg"/>
+        <div class="class-rating">
+          <span class="fa fa-star"></span>
+          <span class="fa fa-star"></span>
+          <span class="fa fa-star"></span>
+          <span class="fa fa-star"></span>
+          <span class="fa fa-star"></span>
         </div>
-        <h1 class="title-feedback rw-title">Lascia un feedback</h1>
         <div class="container-textarea">
-            <textarea id="textareabox" name="textarea1" placeholder="Lascia un feedback..."></textarea>
+            <textarea id="textareabox" placeholder="Lascia un feedback..."></textarea>
         </div>
-        <input type="button" value="Invia" id="inviaFeedback" />
+        <div class="container-button">
+          <input type="button" value="Invia" id="inviaFeedback" />
+        </div>
       </div>`;
-      document.getElementById("close-icon").addEventListener("click", () => {
-        hideContainerCustom();
-        showOrHideChatContainer(conversationContainer, true);
-        showOrHideHeaderContainer(true);
-      });
-    }
-  }
-
-  document.getElementById("inviaFeedback").addEventListener("click", () => {
-    console.log(document.getElementById("textareabox").value);
-  });
-
-  function pilloleSection(conversationContainer, containerCustom) {
-    if (conversationContainer.querySelector(".container-pillole") === null) {
-      if (document.querySelector(".container-custom") === null) {
-        conversationContainer.appendChild(containerCustom);
-        containerCustom.innerHTML = `
-                            <div id="container-custom">
-                                <img class="arrow-left" src="./images/arrow-left.svg"/>
-                            </div>`;
-        document
-          .getElementsByClassName("arrow-left")[0]
-          .addEventListener("click", () => {
-            hideContainerCustom();
-            showOrHideChatContainer(conversationContainer, true);
-          });
-        const pillole = [
-          {
-            title: "Prima pillola",
-            content: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
-          },
-          {
-            title: "Seconda pillola",
-            content: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
-          },
-          {
-            title: "Terza pillola",
-            content: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
-          },
+    handleStarFeedback();
+    document.getElementById("inviaFeedback").addEventListener("click", () => {
+      let textArea = document.getElementById("textareabox");
+      let textAreaValue = textArea.value.trim();
+      if (!textAreaValue) {
+        textArea.classList.add("textarea-error");
+      } else {
+        textArea.classList.remove("textarea-error");
+        const listOfStar = [
+          ...document
+            .getElementsByClassName("class-rating")[0]
+            .getElementsByTagName("span"),
         ];
-        const domPillole = document.createElement("ul");
-        pillole.forEach((pillola) => {
-          const contentLi = document.createElement("div");
-          contentLi.classList.add("container-collapse");
-          contentLi.innerHTML = `
+        const starSelected = listOfStar.filter((star) =>
+          star.classList.contains("checked")
+        ).length;
+        // TODO: allegare la chiamata per il salvataggio del feedback
+        console.log({ textAreaValue, starSelected });
+        backToFirstView();
+      }
+    });
+  }
+}
+
+function handleStarFeedback() {
+  const listOfStar = [
+    ...document
+      .getElementsByClassName("class-rating")[0]
+      .getElementsByTagName("span"),
+  ];
+  listOfStar.forEach((star, index) => {
+    let recordLastStatus = "";
+    star.addEventListener("mouseover", () => {
+      recordLastStatus = "mouseover";
+      starAddChecked(listOfStar.slice(0, index + 1));
+      starRemoveChecked(listOfStar.slice(index + 1, listOfStar.length));
+    });
+    star.addEventListener("mouseout", () => {
+      if (recordLastStatus !== "click") {
+        recordLastStatus = "mouseout";
+        starRemoveChecked(listOfStar);
+      }
+    });
+    star.addEventListener("click", () => {
+      recordLastStatus = "click";
+      listOfStar[index].classList.add("checked");
+    });
+  });
+}
+
+function starRemoveChecked(listOfStar) {
+  listOfStar.forEach((starFiltered) => {
+    starFiltered.classList.remove("checked");
+  });
+}
+
+function starAddChecked(listOfStar) {
+  listOfStar.forEach((starFiltered) => {
+    starFiltered.classList.add("checked");
+  });
+}
+
+function pilloleSection() {
+  if (!conversationContainer.querySelector(".container-pillole")) {
+    if (!document.querySelector(".container-custom")) {
+      createCustomHeader("Pillole");
+      conversationContainer.appendChild(containerCustom);
+      containerCustom.innerHTML = `<div id="container-custom"></div>`;
+      const pillole = [
+        {
+          title: "Prima pillola",
+          content: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+        },
+        {
+          title: "Seconda pillola",
+          content: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+        },
+        {
+          title: "Terza pillola",
+          content: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.`,
+        },
+      ];
+      const domPillole = document.createElement("ul");
+      pillole.forEach((pillola) => {
+        const contentLi = document.createElement("div");
+        contentLi.classList.add("container-collapse");
+        contentLi.innerHTML = `
                                 <button type="button" class="collapsible">${pillola.title}</button>
                                 <div class="content">
                                     <p>${pillola.content}</p>
                                 </div>`;
-          const li = document.createElement("li");
-          li.appendChild(contentLi);
-          domPillole.appendChild(li);
+        const li = document.createElement("li");
+        li.appendChild(contentLi);
+        domPillole.appendChild(li);
+      });
+      [...domPillole.getElementsByTagName("button")].forEach((button) => {
+        button.addEventListener("click", function () {
+          this.classList.toggle("active");
+          var content = this.nextElementSibling;
+          if (content.style.display === "block") {
+            content.style.display = "none";
+          } else {
+            content.style.display = "block";
+          }
         });
-        [...domPillole.getElementsByTagName("button")].forEach((button) => {
-          button.addEventListener("click", function () {
-            this.classList.toggle("active");
-            var content = this.nextElementSibling;
-            if (content.style.display === "block") {
-              content.style.display = "none";
-            } else {
-              content.style.display = "block";
-            }
-          });
-        });
-        document.getElementById("container-custom").appendChild(domPillole);
-      }
+      });
+      document.getElementById("container-custom").appendChild(domPillole);
     }
   }
+}
 
-  function showOrHideChatContainer(conversationContainer, show) {
-    const chatContainer = conversationContainer.getElementsByClassName(
-      "rw-messages-container"
-    )[0];
-    const formContainer = conversationContainer.getElementsByTagName("form")[0];
-    if (show) {
-      chatContainer.classList.remove("display-none");
-      formContainer.classList.remove("display-none");
-    } else {
-      chatContainer.classList.add("display-none");
-      formContainer.classList.add("display-none");
-    }
-  }
+function createCustomHeader(title) {
+  conversationContainer.insertAdjacentHTML(
+    "afterbegin",
+    `
+    <div class="rw-header custom-header">
+      <h4 class="rw-title">${title}</h4>
+      <div class="container-close">
+        <img id="close-icon" src="./images/x-lg.svg" />
+      </div>
+    </div>
+    `
+  );
+  document.getElementById("close-icon").addEventListener("click", () => {
+    backToFirstView();
+  });
+}
 
-  function showOrHideHeaderContainer(show) {
-    const headerContainer = document.getElementsByClassName(
-      "rw-header-and-loading"
-    )[0];
-    if (show) {
-      headerContainer.classList.remove("display-none");
-    } else {
-      headerContainer.classList.add("display-none");
-    }
+function showOrHideChatContainer(show) {
+  const chatContainer = conversationContainer.getElementsByClassName(
+    "rw-messages-container"
+  )[0];
+  const formContainer = conversationContainer.getElementsByTagName("form")[0];
+  if (show) {
+    chatContainer.classList.remove("display-none");
+    formContainer.classList.remove("display-none");
+  } else {
+    chatContainer.classList.add("display-none");
+    formContainer.classList.add("display-none");
   }
+}
 
-  function hideContainerCustom() {
-    document
-      .getElementsByClassName("container-custom")[0]
-      .classList.add("display-none");
+function showOrHideHeaderContainer(show) {
+  const headerContainer = document.getElementsByClassName(
+    "rw-header-and-loading"
+  )[0];
+  if (show) {
+    headerContainer.classList.remove("display-none");
+  } else {
+    headerContainer.classList.add("display-none");
   }
-  // // Avatar title
-  // const avatar = document.createElement('img');
-  // avatar.setAttribute('src', './images/favicon.png');
-  // avatar.setAttribute('alt', 'chat avatar');
-  // avatar.classList.add('rw-avatar');
-  // document.getElementsByClassName('rw-header rw-with-subtitle')[0].insertBefore(avatar, document.getElementsByClassName('rw-header-buttons')[0]);
-}, 500);
+}
+
+function removeContainerCustom() {
+  conversationContainer.removeChild(
+    document.getElementsByClassName("container-custom")[0]
+  );
+}
+
+function removeCustomHeader() {
+  conversationContainer.removeChild(
+    document.getElementsByClassName("custom-header")[0]
+  );
+}
+
+function backToFirstView() {
+  removeContainerCustom();
+  removeCustomHeader();
+  showOrHideChatContainer(true);
+  showOrHideHeaderContainer(true);
+}
+
+// // Avatar title
+// const avatar = document.createElement('img');
+// avatar.setAttribute('src', './images/favicon.png');
+// avatar.setAttribute('alt', 'chat avatar');
+// avatar.classList.add('rw-avatar');
+// document.getElementsByClassName('rw-header rw-with-subtitle')[0].insertBefore(avatar, document.getElementsByClassName('rw-header-buttons')[0]);
